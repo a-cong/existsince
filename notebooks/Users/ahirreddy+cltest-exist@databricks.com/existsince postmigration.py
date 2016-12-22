f97db9b69da9911d7e7c2e845eed4aef745fbb85
Alexandra Cong <a-cong@users.noreply.github.com>
2016-12-21 17:56:39 -0800

    dfsdf

--- notebooks/Users/ahirreddy+cltest-exist@databricks.com/existsince postmigration.py
+++ notebooks/Users/ahirreddy+cltest-exist@databricks.com/existsince postmigration.py
@@ -1,1 +1,18 @@
 # Databricks notebook source
+"hi"
+
+# COMMAND ----------
+
+from pyspark.sql import Row
+
+array = [Row(key="a", group="vowels", value=1),
+         Row(key="b", group="consonants", value=2),
+         Row(key="c", group="consonants", value=3),
+         Row(key="d", group="consonants", value=4),
+         Row(key="e", group="vowels", value=5)]
+dataframe = sqlContext.createDataFrame(sc.parallelize(array))
+
+display(dataframe)
+
+# COMMAND ----------
+
