98ccea01d740807cbc94987abad1321af5d463dc
Alexandra Cong <a-cong@users.noreply.github.com>
2017-03-27 13:56:39 -0700

    Update hi.py

---
+++ notebooks/Users/alexandra+accounts-admin-zt-east@databricks.com/hi.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+x = 1
+
+# COMMAND ----------
+
+x = 2
+
+# COMMAND ----------
+
+"hello"
+
+# COMMAND ----------
+
+sc.parallelize(range(100)).collect()
+
+# COMMAND ----------
+
